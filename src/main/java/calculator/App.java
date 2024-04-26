8791adab7ba3998f758f3ad002db21bd923b8e1c
Robbie <robbie331@>
2024-04-26 11:42:44 +0900

    레벨 2-3이 아닌 2-5까지 complete

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -31,6 +31,7 @@ public class App {
             }
             System.out.println("결과: " + result);
             calc.addResult(result);
+
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
